26a75a13efe7185c808f827ac728aaa26e2af27e
Kharzette <6589537+Kharzette@users.noreply.github.com>
2010-08-27 01:31:45 -0500

    Utility lib for stuff I use in most projects

---
+++ UtilityLib/FileUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace UtilityLib
+{
+	public class FileUtil
+	{
+		public static FileStream OpenTitleFile(string fileName,
+			FileMode mode, FileAccess access)
+		{
+			string	fullPath	=Path.Combine(
+									StorageContainer.TitleLocation,
+									fileName);
+
+			if(!File.Exists(fullPath) &&
+				(access == FileAccess.Write ||
+				access == FileAccess.ReadWrite))
+			{
+				return	File.Create(fullPath);
+			}
+			else
+			{
+				return	File.Open(fullPath, mode, access);
+			}
+		}
+	}
+}
---
+++ UtilityLib/Mathery.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace UtilityLib
+{
+	public class Mathery
+	{
+		public const float VCompareEpsilon	=0.001f;
+
+
+		public static Color RandomColor(Random rnd)
+		{
+			Microsoft.Xna.Framework.Graphics.Color	randColor
+				=new Microsoft.Xna.Framework.Graphics.Color(
+						Convert.ToByte(rnd.Next(255)),
+						Convert.ToByte(rnd.Next(255)),
+						Convert.ToByte(rnd.Next(255)));
+			return	randColor;
+		}
+
+
+		public static float VecIdx(Vector3 v, int idx)
+		{
+			if(idx == 0)
+			{
+				return	v.X;
+			}
+			else if(idx == 1)
+			{
+				return	v.Y;
+			}
+			return	v.Z;
+		}
+
+
+		public static bool CompareVectorEpsilon(Vector3 v1, Vector3 v2, float epsilon)
+		{
+			if((v1.X - v2.X) < -epsilon || (v1.X - v2.X) > epsilon)
+			{
+				return	false;
+			}
+			if((v1.Y - v2.Y) < -epsilon || (v1.Y - v2.Y) > epsilon)
+			{
+				return	false;
+			}
+			if((v1.Z - v2.Z) < -epsilon || (v1.Z - v2.Z) > epsilon)
+			{
+				return	false;
+			}
+			return	true;
+		}
+
+
+		public static void VecIdxAssign(ref Vector3 v, int idx, float val)
+		{
+			if(idx == 0)
+			{
+				v.X	=val;
+			}
+			else if(idx == 1)
+			{
+				v.Y	=val;
+			}
+			else
+			{
+				v.Z	=val;
+			}
+		}
+	}
+}
