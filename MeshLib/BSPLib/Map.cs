d9ff064129ad651b17d62512a12ca207f8f5ad26
Kharzette <6589537+Kharzette@users.noreply.github.com>
2010-12-12 10:11:58 -0600

    Implement extrasamples

--- MeshLib/BSPLib/Map.cs
+++ MeshLib/BSPLib/Map.cs
@@ -530,11 +530,6 @@ namespace BSPLib
 		}
 
 
-		public void RayCast3(Vector3 mStart, Vector3 mEnd, List<Ray> rayParts)
-		{
-		}
-
-
 		bool FixModelTJunctions(FaceFixer ff)
 		{
 			Print(" --- Weld Model Verts --- \n");
@@ -2146,7 +2141,7 @@ namespace BSPLib
 			Vector3	FaceMid;
 			float	MidU, MidV, StartU, StartV, CurU, CurV;
 			Int32	u, v, Width, Height, Leaf;
-			byte	[]InSolid	=new byte[LInfo.MAX_LMAP_SIZE * LInfo.MAX_LMAP_SIZE];
+			bool	[]InSolid	=new bool[LInfo.MAX_LMAP_SIZE * LInfo.MAX_LMAP_SIZE];
 
 			MidU	=(LightInfo.Maxs[0] + LightInfo.Mins[0]) * 0.5f;
 			MidV	=(LightInfo.Maxs[1] + LightInfo.Mins[1]) * 0.5f;
@@ -2178,16 +2173,16 @@ namespace BSPLib
 					//Pre-compute if this point is in solid space, so we can re-use it in the code below
 					if((mGFXLeafs[Leaf].mContents & Contents.BSP_CONTENTS_SOLID2) != 0)
 					{
-						InSolid[(v * Width) + u]	=1;
+						InSolid[(v * Width) + u]	=true;
 					}
 					else
 					{
-						InSolid[(v * Width) + u]	=0;
+						InSolid[(v * Width) + u]	=false;
 					}
 
 					if(!bExtraLightCorrection)
 					{
-						if(InSolid[(v * Width) + u] != 0)
+						if(InSolid[(v * Width) + u])
 						{
 							Vector3	colResult	=Vector3.Zero;
 							if(RayCollisionNoGlobals(FaceMid,
@@ -2207,46 +2202,51 @@ namespace BSPLib
 				return;
 			}
 
-//			pPoint = FaceInfo.Points;
-//			pInSolid = InSolid;
-			/*
-			for (v=0; v< FaceInfo.NumPoints; v++, pPoint++, pInSolid++)
+			for(v=0;v < FaceInfo.NumPoints;v++)
 			{
-				uint8		*pInSolid2;
-				geVec3d		*pPoint2, *pBestPoint;
-				geFloat		BestDist, Dist;
+				float	BestDist, Dist;
 
-				if (!(*pInSolid))
-					continue;						//  Point is good, leav it alone
-
-				pPoint2 = FaceInfo.Points;
-				pInSolid2 = InSolid;
-				pBestPoint = &FaceMid;
-				BestDist = Bounds.MIN_MAX_BOUNDS;
-				
-				for (u=0; u< FaceInfo.NumPoints; u++, pPoint2++, pInSolid2++)
+				if(!InSolid[v])
 				{
-					if (pPoint == pPoint2)			
-						continue;					// We know this point is bad
-
-					if (*pInSolid2)
-						continue;					// We know this point is bad
-
-					// At this point, we have a good point, now see if it's closer than the current good point
-					geVec3d_Subtract(pPoint2, pPoint, &Vect);
-					Dist = geVec3d_Length(&Vect);
-					if (Dist < BestDist)
-					{
-						BestDist = Dist;
-						pBestPoint = pPoint2;
-
-						if (Dist <= (GBSPGlobals.LGRID_SIZE-0.1f))
-							break;					// This should be good enough...
-					}
+					//Point is good, leave it alone
+					continue;
 				}
 
-				*pPoint = *pBestPoint;
-			}*/
+				Vector3	pBestPoint	=FaceMid;
+				BestDist	=Bounds.MIN_MAX_BOUNDS;
+				
+				for(u=0;u < FaceInfo.NumPoints;u++)
+				{
+					if(FaceInfo.Points[v] == FaceInfo.Points[u])
+					{
+						continue;	//We know this point is bad
+					}
+
+					if(InSolid[u])
+					{
+						continue;	// We know this point is bad
+					}
+
+					//At this point, we have a good point,
+					//now see if it's closer than the current good point
+					Vector3	Vect	=FaceInfo.Points[u] - FaceInfo.Points[v];
+					Dist	=Vect.Length();
+					if(Dist < BestDist)
+					{
+						BestDist	=Dist;
+						pBestPoint	=FaceInfo.Points[u];
+
+						if(Dist <= (FInfo.LGRID_SIZE - 0.1f))
+						{
+							break;	//This should be good enough...
+						}
+					}
+				}
+				FaceInfo.Points[v]	=pBestPoint;
+			}
+
+			//free cached vis stuff
+			InSolid	=null;
 		}
 
 
