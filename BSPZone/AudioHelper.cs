d3e0719a2db7fa63cf867cf3de1e2b70882b9a36
Kharzette <6589537+Kharzette@users.noreply.github.com>
2012-12-06 09:08:47 -0600

    Lib docs text file
    audio library in UtilityLib.csproj
    
    audio helper for bsp zones

---
+++ BSPZone/AudioHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using UtilityLib;
+
+
+namespace BSPZone
+{
+	public class AudioHelper
+	{
+		Zone			mZone;
+		Audio			mAudio;
+		AudioListener	mPlayerListener;
+
+		List<ZoneEntity>			mAudioEntities	=new List<ZoneEntity>();
+		List<AudioEmitter>			mEmitters		=new List<AudioEmitter>();
+		List<SoundEffectInstance>	mInstances		=new List<SoundEffectInstance>();
+
+
+		public void Initialize(Zone zone, Audio aud, AudioListener playerListener)
+		{
+			mZone			=zone;
+			mAudio			=aud;
+			mPlayerListener	=playerListener;
+
+			mAudioEntities.Clear();
+			mEmitters.Clear();
+			mInstances.Clear();
+
+			//grab out audio emitters
+			List<ZoneEntity>	sounds	=mZone.GetEntitiesStartsWith("misc_sound");
+			foreach(ZoneEntity ze in sounds)
+			{
+				mAudioEntities.Add(ze);
+
+				Vector3	pos;
+				ze.GetOrigin(out pos);
+
+				AudioEmitter	em	=new AudioEmitter();
+				em.Position	=pos;
+				mEmitters.Add(em);
+
+				string	fxName;
+				fxName	=ze.GetValue("effect_name");
+
+				int		looping;
+				ze.GetInt("looping", out looping);
+
+				float	volume;
+				ze.GetFloat("volume", out volume);
+				
+				SoundEffectInstance	sei	=mAudio.GetInstance(fxName, (looping != 0));
+				sei.Volume	=volume;
+				sei.Apply3D(playerListener, em);
+
+				mInstances.Add(sei);
+
+				string	sOn	=ze.GetValue("turned_on");
+				if(sOn != "0")
+				{
+					sei.Play();
+				}
+			}
+		}
+
+
+		public void Update()
+		{
+			for(int i=0;i < mInstances.Count;i++)
+			{
+				SoundEffectInstance	sei	=mInstances[i];
+				if(sei.State == SoundState.Playing)
+				{
+					sei.Apply3D(mPlayerListener, mEmitters[i]);
+				}
+			}
+		}
+	}
+}
---
+++ UtilityLib/Audio.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+
+namespace UtilityLib
+{
+	public class Audio
+	{
+		Dictionary<string, SoundEffect>	mFX	=new Dictionary<string, SoundEffect>();
+
+		//list of stuff out in the wild, might be playing, might not
+		List<SoundEffectInstance>	mActive	=new List<SoundEffectInstance>();
+
+		//list of stuff this class is managing
+		Dictionary<string, List<SoundEffectInstance>>	mPlayingHere
+			=new Dictionary<string, List<SoundEffectInstance>>();
+
+		const int	MaxInstances	=300;	//xbox limitation
+
+
+		public void AddSound(string name, SoundEffect fx)
+		{
+			if(!mFX.ContainsKey(name))
+			{
+				mFX.Add(name, fx);
+			}
+		}
+
+
+		public SoundEffectInstance GetInstance(string name, bool bLooping)
+		{
+			if(!mFX.ContainsKey(name) || mActive.Count >= MaxInstances)
+			{
+				return	null;
+			}
+
+			SoundEffect	se	=mFX[name];
+
+			SoundEffectInstance	sei	=se.CreateInstance();
+
+			sei.IsLooped	=bLooping;
+
+			mActive.Add(sei);
+
+			return	sei;
+		}
+
+
+		public void ReleaseAll()
+		{
+			List<SoundEffectInstance>	toNuke	=new List<SoundEffectInstance>(mActive);
+
+			foreach(SoundEffectInstance sei in toNuke)
+			{
+				ReleaseInstance(sei);
+			}
+
+			Debug.Assert(mActive.Count == 0);
+
+			mPlayingHere.Clear();
+		}
+
+
+		public void ReleaseInstance(SoundEffectInstance sei)
+		{
+			if(sei == null)
+			{
+				return;
+			}
+
+			mActive.Remove(sei);
+
+			sei.Stop(true);
+			sei.Dispose();
+		}
+
+
+		public int GetNumInstances()
+		{
+			return	mActive.Count();
+		}
+
+
+		public void Update()
+		{
+			foreach(KeyValuePair<string, List<SoundEffectInstance>> seis in mPlayingHere)
+			{
+				for(int i=0;i < seis.Value.Count;i++)
+				{
+					if(seis.Value[i].State == SoundState.Stopped)
+					{
+						ReleaseInstance(seis.Value[i]);
+						seis.Value.RemoveAt(i);
+						i--;
+					}
+				}
+			}
+		}
+
+
+		public void StopSound(string name)
+		{
+			if(!mPlayingHere.ContainsKey(name))
+			{
+				return;
+			}
+
+			foreach(SoundEffectInstance sei in mPlayingHere[name])
+			{
+				sei.Stop();
+			}
+		}
+
+
+		//tracked in this class
+		public void Play(string name, bool bLooping, float volume, float pan)
+		{
+			if(!mFX.ContainsKey(name) || mActive.Count >= MaxInstances)
+			{
+				return;
+			}
+
+			SoundEffect	se	=mFX[name];
+
+			SoundEffectInstance	sei	=se.CreateInstance();
+
+			//todo:  This looks like maybe a race condition could happen
+			lock(mActive)
+			{
+				mActive.Add(sei);
+			}
+			lock(mPlayingHere)
+			{
+				if(mPlayingHere.ContainsKey(name))
+				{
+					mPlayingHere[name].Add(sei);
+				}
+				else
+				{
+					mPlayingHere.Add(name, new List<SoundEffectInstance>());
+					mPlayingHere[name].Add(sei);
+				}
+			}
+
+			sei.Pan			=pan;
+			sei.Volume		=volume;
+			sei.IsLooped	=bLooping;
+			sei.Play();
+		}
+
+
+		//tracked in this class
+		public void Play(string name, bool bLooping, float volume)
+		{
+			Play(name, bLooping, volume, 0.0f);
+		}
+	}
+}
