bd0ce246f7b9ea3858bf8ea85f4487a013928a5b
Kharzette <6589537+Kharzette@users.noreply.github.com>
2012-12-13 22:02:08 -0600

    IntermissionHelper class for pulling out camera values

---
+++ BSPZone/IntermissionHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using UtilityLib;
+
+
+namespace BSPZone
+{
+	public class IntermissionHelper
+	{
+		Zone	mZone;
+
+		//data
+		List<ZoneEntity>	mIMEntities	=new List<ZoneEntity>();
+
+
+		public void Initlialize(Zone zone)
+		{
+			mZone	=zone;
+
+			mIMEntities	=mZone.GetEntitiesStartsWith("info_player_intermission");
+		}
+
+
+		public void GetRandomIntermissionData(Random mRand, out Vector3 pos,
+			out int pitch, out int yaw, out int roll)
+		{
+			//defaults
+			pos		=Vector3.Zero;
+			yaw		=0;
+			pitch	=0;
+			roll	=0;
+
+			int	choice	=mRand.Next(0, mIMEntities.Count);
+
+			ZoneEntity	ze	=mIMEntities[choice];
+
+			ze.GetOrigin(out pos);
+			Vector3	orient;
+			if(ze.GetVectorNoConversion("angles", out orient))
+			{
+				//coordinate system goblinry
+				yaw		=(int)-orient.Y - 90;
+				pitch	=(int)orient.X;
+				roll	=(int)orient.Z;
+			}
+			else if(ze.GetVectorNoConversion("mangles", out orient))
+			{
+				//coordinate system goblinry
+				yaw		=(int)-orient.Y - 90;
+				pitch	=(int)orient.X;
+				roll	=(int)orient.Z;
+			}
+		}
+	}
+}
