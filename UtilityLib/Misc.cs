8cc0ec75290a9c4e237d6d3e6a23da1c645bd3dd
Kharzette <6589537+Kharzette@users.noreply.github.com>
2013-02-14 02:34:19 -0600

    Split out some non xna specific code and made a UtilityLib.csproj for vs2012

--- UtilityLib/Misc.cs
+++ UtilityLib/Misc.cs
@@ -7,35 +7,8 @@ using Microsoft.Xna.Framework;
 
 namespace UtilityLib
 {
-	public static class Misc
+	public static partial class Misc
 	{
-		public static void SafeInvoke(this EventHandler eh, object sender)
-		{
-			if(eh != null)
-			{
-				eh(sender, EventArgs.Empty);
-			}
-		}
-
-
-		public static void SafeInvoke(this EventHandler eh, object sender, EventArgs ea)
-		{
-			if(eh != null)
-			{
-				eh(sender, ea);
-			}
-		}
-
-
-		public static void SafeInvole<T>(this EventHandler<T> eh, object sender, T ea) where T : EventArgs
-		{
-			if(eh != null)
-			{
-				eh(sender, ea);
-			}
-		}
-
-
 		public static Vector4 ARGBToVector4(int argb)
 		{
 			Vector4	ret	=Vector4.Zero;
@@ -77,28 +50,6 @@ namespace UtilityLib
 				mag	=inVec.Z;
 			}
 			return	inVec / mag;
-		}
-
-
-		static string AddCountStuffToString(int num, string stuff)
-		{
-			string	ret	="";
-
-			for(int i=0;i < num;i++)
-			{
-				ret	+=stuff;
-			}
-			return	ret;
-		}
-
-
-		public static string FloatToString(float f, int numDecimalPlaces)
-		{
-			//this I think prevents scientific notation on small numbers
-			decimal	d	=Convert.ToDecimal(f);
-
-			return	string.Format("{0:0." + AddCountStuffToString(numDecimalPlaces, "#") + "}",
-				d.ToString(System.Globalization.CultureInfo.InvariantCulture));
 		}
 
 
@@ -182,30 +133,6 @@ namespace UtilityLib
 			ret.Max.Z	=halfDepth;
 
 			return	ret;
-		}
-
-
-		public static bool bFlagSet(UInt32 val, UInt32 flag)
-		{
-			return	((val & flag) != 0);
-		}
-
-
-		public static bool bFlagSet(Int32 val, Int32 flag)
-		{
-			return	((val & flag) != 0);
-		}
-
-
-		public static void ClearFlag(ref Int32 val, Int32 flag)
-		{
-			val	&=(~flag);
-		}
-
-
-		public static void ClearFlag(ref UInt32 val, UInt32 flag)
-		{
-			val	&=(~flag);
 		}
 	}
 }
---
+++ UtilityLib/MiscNoXNA.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib
+{
+	public static partial class Misc
+	{
+		public static void SafeInvoke(this EventHandler eh, object sender)
+		{
+			if(eh != null)
+			{
+				eh(sender, EventArgs.Empty);
+			}
+		}
+
+
+		public static void SafeInvoke(this EventHandler eh, object sender, EventArgs ea)
+		{
+			if(eh != null)
+			{
+				eh(sender, ea);
+			}
+		}
+
+
+		public static void SafeInvole<T>(this EventHandler<T> eh, object sender, T ea) where T : EventArgs
+		{
+			if(eh != null)
+			{
+				eh(sender, ea);
+			}
+		}
+
+
+		static string AddCountStuffToString(int num, string stuff)
+		{
+			string	ret	="";
+
+			for(int i=0;i < num;i++)
+			{
+				ret	+=stuff;
+			}
+			return	ret;
+		}
+
+
+		public static string FloatToString(float f, int numDecimalPlaces)
+		{
+			//this I think prevents scientific notation on small numbers
+			decimal	d	=Convert.ToDecimal(f);
+
+			return	string.Format("{0:0." + AddCountStuffToString(numDecimalPlaces, "#") + "}",
+				d.ToString(System.Globalization.CultureInfo.InvariantCulture));
+		}
+
+
+		public static bool bFlagSet(UInt32 val, UInt32 flag)
+		{
+			return	((val & flag) != 0);
+		}
+
+
+		public static bool bFlagSet(Int32 val, Int32 flag)
+		{
+			return	((val & flag) != 0);
+		}
+
+
+		public static void ClearFlag(ref Int32 val, Int32 flag)
+		{
+			val	&=(~flag);
+		}
+
+
+		public static void ClearFlag(ref UInt32 val, UInt32 flag)
+		{
+			val	&=(~flag);
+		}
+	}
+}
