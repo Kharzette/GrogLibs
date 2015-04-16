b58323f562cd5f576ab5b8a6648be08d886431ac
Kharzette <6589537+Kharzette@users.noreply.github.com>
2015-04-16 09:15:32 -0500

    User settings module for UtilityLib

---
+++ UtilityLib/UserSettings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UtilityLib
+{
+	public class UserSettings
+	{
+		float	mMouseTurnMultiplier	=0.13f;
+		float	mAnalogTurnMultiplier	=0.5f;
+		float	mKeyTurnMultiplier		=0.5f;
+
+		public int	mTurnSensitivity	=5;
+		public bool	mbYAxisInverted		=false;
+		public bool	mbMultiSampling		=false;
+		public bool	mbFullScreen		=false;
+
+
+		public UserSettings()
+		{
+			ReadSettings();
+		}
+
+
+		public void SaveSettings()
+		{
+			FileStream		fs	=new FileStream("ControlSettings.sav", FileMode.Create, FileAccess.Write);
+			BinaryWriter	bw	=new BinaryWriter(fs);
+
+			bw.Write(mTurnSensitivity);
+			bw.Write(mbYAxisInverted);
+			bw.Write(mbMultiSampling);
+			bw.Write(mbFullScreen);
+
+			bw.Close();
+			fs.Close();
+		}
+
+
+		void ReadSettings()
+		{
+			if(!File.Exists("ControlSettings.sav"))
+			{
+				return;
+			}
+
+			FileStream		fs	=new FileStream("ControlSettings.sav", FileMode.Open, FileAccess.Read);
+			BinaryReader	br	=new BinaryReader(fs);
+
+			mTurnSensitivity	=br.ReadInt32();
+			mbYAxisInverted		=br.ReadBoolean();
+			mbMultiSampling		=br.ReadBoolean();
+			mbFullScreen		=br.ReadBoolean();
+
+			br.Close();
+			fs.Close();
+		}
+	}
+}
