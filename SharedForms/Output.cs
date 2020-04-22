9df86d4c33c8fae9608cdb13312d5781bf56889e
Kharzette <6589537+Kharzette@users.noreply.github.com>
2020-04-22 07:54:10 -0500

    Moved the output form from the bsp builder project out to shared forms so other stuff can use it.  I really needed something like it in the collada stuff to print warnings and such.

---
+++ SharedForms/Output.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace SharedForms
+{
+	public partial class Output : Form
+	{
+		public Output() : base()
+		{
+			InitializeComponent();
+		}
+
+
+		public void Print(string toPrint)
+		{
+			Action<TextBox>	ta	=con => con.AppendText(toPrint);
+			FormExtensions.Invoke(ConsoleOut, ta);
+		}
+
+
+		public void UpdateProgress(int min, int max, int cur)
+		{
+			if(cur < min)
+			{
+				cur	=min;
+			}
+			else if(cur > max)
+			{
+				cur	=max;
+			}
+
+			Action<ProgressBar>	pmin	=prog => prog.Minimum = min;
+			Action<ProgressBar>	pmax	=prog => prog.Maximum = max;
+			Action<ProgressBar>	pcur	=prog => prog.Value = cur;
+
+			SharedForms.FormExtensions.Invoke(BuildProgress, pmin);
+			SharedForms.FormExtensions.Invoke(BuildProgress, pmax);
+			SharedForms.FormExtensions.Invoke(BuildProgress, pcur);
+		}
+	}
+}
---
+++ SharedForms/Output.designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SharedForms
+{
+	partial class Output
+	{
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.BuildProgress = new System.Windows.Forms.ProgressBar();
+			this.ConsoleOut = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// BuildProgress
+			// 
+			this.BuildProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.BuildProgress.Location = new System.Drawing.Point(12, 12);
+			this.BuildProgress.Name = "BuildProgress";
+			this.BuildProgress.Size = new System.Drawing.Size(634, 19);
+			this.BuildProgress.TabIndex = 25;
+			// 
+			// ConsoleOut
+			// 
+			this.ConsoleOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.ConsoleOut.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ConsoleOut.Location = new System.Drawing.Point(12, 37);
+			this.ConsoleOut.Multiline = true;
+			this.ConsoleOut.Name = "ConsoleOut";
+			this.ConsoleOut.ReadOnly = true;
+			this.ConsoleOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.ConsoleOut.Size = new System.Drawing.Size(634, 266);
+			this.ConsoleOut.TabIndex = 26;
+			this.ConsoleOut.TabStop = false;
+			// 
+			// Output
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(658, 315);
+			this.ControlBox = false;
+			this.Controls.Add(this.ConsoleOut);
+			this.Controls.Add(this.BuildProgress);
+			this.Name = "Output";
+			this.Text = "Output";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ProgressBar BuildProgress;
+		private System.Windows.Forms.TextBox ConsoleOut;
+	}
+}
